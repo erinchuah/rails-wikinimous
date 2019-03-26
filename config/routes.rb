1bc38e9a273135fa96520623322027558155b3e0
Erin Chuah <chuah.erin@gmail.com>
2019-03-26 23:35:49 +0800

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
+  resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all if Rails.env.development?
+
+10.times do
+  Article.create!(title: Faker::Coffee.blend_name, content: Faker::GreekPhilosophers.quote)
+end
+
+puts "Created #{Article.count} articles"
